e09c64347c25dcfb630befa13a0b007cf7ca30da
ChrisBenetto <chrisbenetto@gmail.com>
2021-07-30 08:32:06 +0200

    Profil Page and Edit Form

--- src/Form/FigurineType.php
+++ src/Form/FigurineType.php
@@ -9,21 +9,31 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class FigurineType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description')
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la figurine',
+                'help' => 'Nom officiel de la figurine',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'help' => 'Vous pouvez décrire les choix de peintures ainsi que les techniques utilisées , vos difficultés et vos réussites ...',
+            ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
+                'label' => 'Categorie',
                 'choice_label' => 'name',
                 'multiple' => false,
                 'expanded' => true,
             ])
             ->add('pictures', FileType::class, [
+                'label' => 'Images',
                 'multiple' => true,
                 'mapped' => false
             ]);
